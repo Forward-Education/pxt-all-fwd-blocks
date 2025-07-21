729fe8c4e4f09f12de4aacc182ac43812895ca43
ssande-fwd <ssande@forwardedu.com>
2025-07-21 11:23:17 -0400

    full block compilation testing

--- tests/test.ts
+++ tests/test.ts
@@ -1,72 +1,87 @@
-// This extension is quite big to write comprehensive tests for. Here are some compilation tests to start.
-// It already has coverage via the individual kit extensions.
-// Any multi-kit tutorials will be hosted here and the final code for those tutorials will be added as test files
-
-// lights tests
-// setBrightness(value: number)
-basic.forever(() => {
-    console.log("Test Start")
-    console.log(
-        "The brightness is changing every second for 6 seconds, but the Jacdac simulator only shows on / off."
-    )
-    basic.pause(1000)
-    fwdLights.lights1.setBrightness(100)
-    console.log("Light on? " + fwdLights.lights1.isOn())
-    basic.pause(1000)
-    fwdLights.lights1.setBrightness(75)
-    basic.pause(1000)
-    fwdLights.lights1.setBrightness(50)
-    basic.pause(1000)
-    fwdLights.lights1.setBrightness(25)
-    basic.pause(1000)
-    fwdLights.lights1.setBrightness(10)
-    basic.pause(1000)
-    fwdLights.lights1.setBrightness(0)
-    console.log("Light on? " + fwdLights.lights1.isOn())
-    console.log("Test End")
+fwdButtons.dial1.onRotated(
+    fwdEnums.ClockwiseCounterclockwise.Clockwise,
+    function () {
+        basic.showNumber(fwdButtons.dial1.position())
+    }
+)
+fwdSensors.line1.onLineSensorStateChange(function () {
+    if (fwdSensors.line1.isLineSensorState(fwdEnums.OnOff.Off)) {
+        basic.showNumber(fwdSensors.line1.lineSensorState())
+    }
 })
-fwdSensors.float1.onFloatChange(fwdEnums.RaisedLowered.Raised, () => {
-    console.log("Event: raised")
+fwdButtons.dialButton1.onEvent(jacdac.ButtonEvent.Down, function () {
+    if (fwdButtons.dialButton1.isPressed()) {
+        basic.showNumber(fwdButtons.dialButton1.holdDuration())
+    }
 })
-fwdSensors.float1.onFloatChange(fwdEnums.RaisedLowered.Lowered, () => {
-    console.log("Event: lowered")
+fwdButtons.touch1.onEvent(jacdac.ButtonEvent.Down, function () {
+    if (fwdButtons.touch1.isPressed()) {
+        basic.showNumber(fwdButtons.touch1.holdDuration())
+    }
 })
-basic.forever(() => {
+fwdSensors.float1.onFloatChange(fwdEnums.RaisedLowered.Raised, function () {
     if (
         fwdSensors.float1.floatStateConditional(fwdEnums.RaisedLowered.Raised)
     ) {
-        console.log("State: " + fwdSensors.float1.floatState())
+        basic.showNumber(fwdSensors.float1.floatState())
     }
-    if (
-        fwdSensors.float1.floatStateConditional(fwdEnums.RaisedLowered.Lowered)
-    ) {
-        console.log("State: " + fwdSensors.float1.floatState())
-    }
-    basic.pause(1000)
 })
-
-// dial tests
-// position()
-// onRotated(direction: DialDirection, handler: () => void): void
-fwdButtons.dial1.onRotated(fwdEnums.ClockwiseCounterclockwise.Clockwise, () =>
-    console.log("turned -> position " + fwdButtons.dial1.position())
-)
-fwdButtons.dial1.onRotated(
-    fwdEnums.ClockwiseCounterclockwise.Counterclockwise,
-    () => console.log("turned <- position " + +fwdButtons.dial1.position())
-)
-
-// button tests
-// onEvent(event: jacdac.ButtonEvent, handler: () => void)
-// holdDuration(): number
-// isPressed(): boolean
-console.log("Button pressed? " + fwdButtons.dialButton1.isPressed())
-fwdButtons.dialButton1.onEvent(jacdac.ButtonEvent.Down, () =>
-    console.log("dialdown")
-)
-fwdButtons.dialButton1.onEvent(jacdac.ButtonEvent.Up, () =>
-    console.log("dialup")
-)
-fwdButtons.dialButton1.onEvent(jacdac.ButtonEvent.Hold, () =>
-    console.log("Hold Duration: " + fwdButtons.dialButton1.holdDuration())
-)
+fwdButtons.BTN1.onEvent(jacdac.ButtonEvent.Down, function () {
+    if (fwdButtons.BTN1.isPressed()) {
+        basic.showNumber(fwdButtons.BTN1.holdDuration())
+    }
+})
+if (fwdSensors.current1.isPastThreshold(0, fwdEnums.OverUnder.Over)) {
+    basic.showNumber(fwdSensors.current1.current())
+}
+if (fwdSensors.voltage1.isPastThreshold(0, fwdEnums.OverUnder.Over)) {
+    basic.showNumber(fwdSensors.voltage1.voltage())
+}
+fwdSensors.ph1.calibrate(0, 0, 0, 0)
+if (fwdSensors.ph1.isPastThreshold(0, fwdEnums.OverUnder.Over)) {
+    basic.showNumber(fwdSensors.ph1.ph())
+}
+if (fwdSensors.solar1.isPastThreshold(5, fwdEnums.OverUnder.Over)) {
+    basic.showNumber(fwdSensors.solar1.lightLevel())
+}
+if (fwdSensors.sonar1.fwdDistancePastThreshold(0, fwdEnums.OverUnder.Over)) {
+    basic.showNumber(fwdSensors.sonar1.fwdDistance())
+}
+if (fwdSensors.temperature1.isPastThreshold(0, fwdEnums.OverUnder.Over)) {
+    basic.showNumber(fwdSensors.temperature1.temperature())
+}
+if (fwdMotors.pump.isOn()) {
+    fwdMotors.pump.timedRun(500)
+    fwdMotors.pump.setOn(false)
+}
+if (fwdMotors.conIsEnabled(fwdBase.leftServo)) {
+    fwdMotors.setSpeed(fwdBase.leftServo, 0)
+    basic.showNumber(fwdMotors.getSpeed(fwdBase.leftServo))
+    fwdMotors.conSetEnabled(fwdBase.leftServo, false)
+}
+fwdMotors.setupDriving(fwdBase.leftServo, fwdBase.leftServo)
+fwdMotors.drive(fwdEnums.ForwardReverse.Forward, 50)
+fwdMotors.stop()
+fwdMotors.turn(0)
+if (fwdMotors.posIsEnabled(fwdBase.leftServo)) {
+    fwdMotors.setAngle(
+        fwdBase.leftServo,
+        fwdMotors.positionPresets(fwdMotors.ServoClockPositions.Position0)
+    )
+    fwdMotors.setAngleAndWait(fwdBase.leftServo, 0)
+    fwdMotors.posSetEnabled(fwdBase.leftServo, false)
+    basic.showNumber(fwdMotors.getAngle(fwdBase.leftServo))
+}
+fwdLights.ledRing1.setPixelColor(fwdLights.LEDRingPixels.Pixel1, 0xff0000)
+fwdLights.ledRing1.setAllPixelsColor(0xff0000)
+fwdLights.ledRing1.setBrightness(10)
+fwdLights.ledRing1.rotate(1)
+fwdLights.ledRing1.shift(1)
+basic.showNumber(fwdLights.ledRing1.brightness())
+if (fwdLights.lights1.isOn()) {
+    fwdLights.lights1.setBrightness(0)
+}
+if (fwdLights.GREEN.isOn()) {
+    fwdLights.GREEN.setOnOff(false)
+}
+basic.forever(function () {})
